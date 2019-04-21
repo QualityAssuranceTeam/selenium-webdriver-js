c361aa9c3ce2e065db25f91c91bc0ac43d08f0af
yahmedov <ahmedov@officernd.com>
2019-04-21 23:03:40 +0300

    implement navigation in page constructor

---
+++ pom/frame.js
@@ -0,0 +1,30 @@
+//const Page = require('./page');
+const { Builder, Key, By, until } = require('selenium-webdriver');
+
+class Frame {
+    get headerMenu() {
+        return {
+            signOnButton: By.xpath('//a[.=\'SIGN-ON\']'),
+            registerButton: By.xpath('//a[.=\'REGISTER\']'),
+            signOffButton: By.xpath('//a[.=\'SIGN-OFF\']'),
+            itineraryButton: By.xpath('//a[.=\'ITINERARY\']'),
+            profileButton: By.xpath('//a[.=\'PROFILE\']'),
+            supportButton: By.xpath('//a[.=\'SUPPORT\']'),
+            contactButton: By.xpath('//a[.=\'CONTACT\']')
+        }
+    }
+
+    get sideMenu() {
+        return {
+            homeButton: By.xpath('//a[.=\'Home\')]'),
+            flightsButton: By.xpath('//a[.=\'Flights\')]'),
+            hotelsButton: By.xpath('//a[.=\'Hotels\')]'),
+            carRentalsButton: By.xpath('//a[.=\'Car Rentals\')]'),
+            cruisesButton: By.xpath('//a[.=\'Cruises\')]'),
+            destinationsButton: By.xpath('//a[.=\'Destinations\')]'),
+            vacationsButton: By.xpath('//a[.=\'Vacations\')]')
+        };
+    }
+}
+
+module.exports = Frame;
--- pom/page.js
+++ pom/page.js
@@ -1,7 +1,10 @@
+const Frame = require('./frame');
+
 class Page {
     constructor(driver) {
         this.driver = driver;
         this.baseUrl = 'http://newtours.demoaut.com';
+        this.navigation = new Frame();
     }
 
     async goTo(path = '/') {
@@ -11,6 +14,10 @@ class Page {
     async enterText(target, value) {
         await this.driver.findElement(target).click();
         await this.driver.findElement(target).sendKeys(value);
+    }
+
+    async clickOn(target) {
+        await this.driver.findElement(target).click();
     }
 }
 
